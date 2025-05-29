cb8dcfc28f80bcf5ab9592e8e32dc267769e6ced
Fifaliana_valea <159806408+fifaliana-valea@users.noreply.github.com>
2025-05-29 09:33:30 +0300

    Initial commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace ERPNextNewApp.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
