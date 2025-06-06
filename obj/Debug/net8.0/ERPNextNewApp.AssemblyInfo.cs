fddb83c98bb5d27515a757fca1da32f12a5966b3
Fifaliana_valea <159806408+fifaliana-valea@users.noreply.github.com>
2025-06-06 10:21:03 +0300

    modification affichage graphe

--- obj/Debug/net8.0/ERPNextNewApp.AssemblyInfo.cs
+++ obj/Debug/net8.0/ERPNextNewApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ERPNextNewApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+463f3ebb4794beebb03db6d865b2c5c97b58878f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6a301015b8605ce1fbeb2a2be60fc13d3b5182e0")]
 [assembly: System.Reflection.AssemblyProductAttribute("ERPNextNewApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ERPNextNewApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
